704fa8877a880e8eda2d65026a5db65952c24f85
gustavohwenre <130365649+gustavohwenre@users.noreply.github.com>
2023-04-11 14:46:18 -0300

    sexo gostoso

---
+++ cu.js
@@ -0,0 +1,4 @@
+$("#mudar_imagem").click(function(){
+    $("#imagem_js").attr("src", "/cleiton.jpg");
+    $("#mudar_imagem").hide();
+});
---
+++ cuzin.js
@@ -0,0 +1,13 @@
+document.getElementById("click-me").onmouseover = function() {
+    alert('facebok: gusTavz  ');
+};
+document.getElementById("click-m").onmouseover = function() {
+    alert('instagram: @Gusutabz');
+};
+document.getElementById("click-").onmouseover = function() {
+    alert('tiktok: gusTavoz');
+};
+
+document.getElementById("clic").onmouseover = function() {
+    alert('twitter: Gustavoozz');
+};
